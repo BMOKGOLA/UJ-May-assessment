641ab1b41f269a7dfaae681f6cd86f0fadf7185f
BMokgola <boitumelomokgola1@gmail.com>
2024-05-30 15:43:21 +0200

    index.html

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import handlebars from 'vite-plugin-handlebars';
 import data from './data.json';
 
 export default {
-  plugins: [
-    handlebars({
-      context: {}, // replace the curly brackets with data.json
-    }),
-  ],
+  plugins: [handlebars({
+      context: data
+    })],
 };
